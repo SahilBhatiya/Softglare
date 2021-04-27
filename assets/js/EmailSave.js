35e2fb3f50865fce1db62918eb5768208f9a11d0
Apotheosis Mind <64317865+ApotheosisMind@users.noreply.github.com>
2021-04-27 19:49:07 +0530

    Created SendMessage

--- assets/js/EmailSave.js
+++ assets/js/EmailSave.js
@@ -1,6 +1,21 @@
 const EmailControl = document.getElementById("Email");
 
-async function SendEmail() {
-    let Email = EmailControl.value;
-    alert(Email);
-}
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxjn-FPSg6sOkeAeJmoTnmQVfSSUMZleX9qXA0BdNSLWjeCBMhL/exec'
+
+const form = document.getElementById('contact');
+const btnForm = document.getElementById('submit-btn');
+let MsgNumber = 0;
+
+btnForm.addEventListener('click', e => {
+    e.preventDefault()
+    btnForm.value = "Please Wait";
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form),
+        })
+        .then(response => {
+            btnForm.value = "Submit";
+            EmailControl.value = "";
+        })
+        .catch(error => btnForm.value = "Error")
+})
